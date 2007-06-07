77e7e63d7f413f13b36e7f90ed0ff3686656df54
James Dunne <james.jdunne@gmail.com>
2007-06-07 22:32:39 +0000

    Joe's being whiny - moved the PIC backup x0?? folders to /tags/PIC backups/.
    git-svn-id: svn://localhost/e-minor-controller/trunk@37 81a870ca-24fd-4f65-b8a6-cbdfd7b1111e

--- PIC/x014/types.h
+++
@@ -1,7 +0,0 @@
-/* common typedefs used throughout the code */
-typedef unsigned char   u8;
-typedef signed char     s8;
-typedef unsigned short  u16;
-typedef signed short    s16;
-typedef unsigned int    u32;
-typedef signed int      s32;
